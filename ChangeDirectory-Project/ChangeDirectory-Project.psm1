4d04a0895e414d6b66a740b95b8991b53da3193e
Jeremia Axel <jeremiaaxelbachtera@gmail.com>
2022-10-23 19:41:25 +0700

    Init to Git

---
+++ ChangeDirectory-Project/ChangeDirectory-Project.psm1
@@ -0,0 +1,63 @@
+function ChangeDirectory-Project() { 
+    <#
+        .SYNOPSIS
+        Change directory into the specified directory keys
+
+        .PARAMETER target
+        Specifies target project
+        .PARAMETER -list
+        Print the list of available mappings to directories
+
+        .EXAMPLE
+        PS> cdprj school
+    #>
+    [Alias('cdprj')]
+
+    param (
+        [string]$target = $null,
+        [switch]$list
+    )
+    
+    function printAvailableKeys([hashtable]$keyToProjectMap) {
+        $maxLength = 0;
+        foreach($item in $keyToProjectMap.GetEnumerator()){
+            if ($item.Key.Length -gt $maxLength)
+            {
+                $maxLength = $item.Key.Length;
+            }
+        }
+
+        "Available keys are:";
+        foreach($item in $keyToProjectMap.GetEnumerator()){
+            "{0, -$maxLength} : {1}" -f "$($item.key)", "$($item.value)"
+        }
+    }
+    
+    $basePath = "C:\Users\jerem\";
+    $result = "";
+    $defaultKey = 'school';
+    $keyToProjectMap = @{ 
+        'school' = $basePath + 'SchoolProjects';
+        'sch' = $basePath + 'SchoolProjects';
+        
+        'personal' = $basePath + 'PersonalProjects';
+        'self' = $basePath + 'PersonalProjects';
+        
+        'work' = $basePath + 'WorkProjects';
+        'config' = (Get-Item $PROFILE).Directoryname;
+    }
+
+    if ($list) {
+        printAvailableKeys($keyToProjectMap);
+        if (!$target) { return; }
+    }
+
+    if (!$keyToProjectMap.ContainsKey($target)) {
+        "Key `"$target`" does not exist in the mappings.";
+        "``cdprj -list`` for the list of keys.";
+        "Defaulting to $defaultKey";
+    } 
+    $result += ($keyToProjectMap[$target] ?? $keyToProjectMap[$defaultKey]);
+    cd $result;
+}
+Export-ModuleMember -Function ChangeDirectory-Project -Alias cdprj
---
+++ GitAliasesModule/GitAliasesModule.psm1
@@ -0,0 +1,91 @@
+### Git Aliases
+function Git-Clone() { 
+    [Alias('gcln')]
+    param($params)
+    git clone $params;
+}
+Export-ModuleMember -Function Git-Clone -Alias gcln
+
+function Git-Status() { 
+    [Alias('gst')]
+    param($params)
+    git status $params;
+}
+Export-ModuleMember -Function Git-Status -Alias gst
+
+function Git-Commit() { 
+    [Alias('gcm')]
+    param($params)
+    git commit $params;
+}
+Export-ModuleMember -Function Git-Commit -Alias gcmt
+
+function Git-CommitMessage() { 
+    [Alias('gcmsg')]
+    param($params)
+    git commit -m $params;
+}
+Export-ModuleMember -Function Git-CommitMessage -Alias gcmsg
+
+function Git-Push() { 
+    [Alias('gpsh')]
+    param($params)
+    git push $params;
+}
+Export-ModuleMember -Function Git-Push -Alias gpsh
+
+function Git-Pull() { 
+    [Alias('gpll')]
+    param($params)
+    git pull $params;
+}
+Export-ModuleMember -Function Git-Pull -Alias gpll
+
+function Git-Checkout() { 
+    [Alias('gco')]
+    param($params)
+    git checkout $params;
+}
+Export-ModuleMember -Function Git-Checkout -Alias gco
+
+function Git-Branch() { 
+    [Alias('gb')]
+    param($params)
+    git branch $params;
+}
+Export-ModuleMember -Function Git-Branch -Alias gb
+
+function Git-Restore() { 
+    [Alias('grst')]
+    param($params)
+    git restore $params;
+}
+Export-ModuleMember -Function Git-Restore -Alias grst
+
+function Git-Log() { 
+    [Alias('glog')]
+    param($params)
+    git log $params;
+}
+Export-ModuleMember -Function Git-Log -Alias glog
+
+function Git-Add() { 
+    [Alias('ga')]
+    param($params)
+    git add $params;
+}
+Export-ModuleMember -Function Git-Add -Alias ga
+
+function Git-AddAll() { 
+    [Alias('gaa')]
+    param($params)
+    git add .;
+}
+Export-ModuleMember -Function Git-AddAll -Alias gaa
+
+function Git-Diff() { 
+    [Alias('gdiff')]
+    param($params)
+    git diff $params;
+}
+Export-ModuleMember -Function Git-Diff -Alias gdiff
